95c0b67ff03195e29d763f7e97dd18bde1810c84
s123dhara <spdh427@gmail.com>
2024-10-15 11:38:57 +0530

    LC:2938 Separate Black and White Balls

---
+++ String/Seperate_Black_and_white_Balls.java
@@ -0,0 +1,34 @@
+/**
+ * Seperate_Black_and_white_Balls
+ */
+public class Seperate_Black_and_white_Balls {
+
+    public static long minimumSteps(String s) {
+
+        int n = s.length();
+        long ans = 0;
+        long ones_count = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            char c = s.charAt(i);
+            if(c == '1') ones_count += 1;
+
+            if(c == '0') {
+                ans += ones_count;
+            }
+
+        }
+
+        return ans;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        // String s = "100";
+        String s = "0111";
+        System.out.println("OUPUT : "+minimumSteps(s));
+    }
+}
