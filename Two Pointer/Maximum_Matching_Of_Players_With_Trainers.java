8a3a88524a19a0a2531740cc7ccb8cffd2af0f6d
s123dhara <spdh427@gmail.com>
2025-07-13 10:14:14 +0530

    LC:2410. Maximum Matching of Players With Trainers

---
+++ Two Pointer/Maximum_Matching_Of_Players_With_Trainers.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Maximum_Matching_Of_Players_With_Trainers {
+
+    public int matchPlayersAndTrainers(int[] players, int[] trainers) {
+        Thread t1 = new Thread(() -> Arrays.sort(players));
+        Thread t2 = new Thread(() -> Arrays.sort(trainers));
+        t1.start();
+        t2.start();
+        try {
+            t1.join(); // Waits Until t1 Thread work done.
+            t2.join(); // Waits Until t2 Thread work done.
+        } catch (Exception e) {
+        }
+
+        int n = players.length;
+        int m = trainers.length;
+
+        int i = n - 1;
+        int j = m - 1;
+
+        int result = 0;
+        while (j >= 0 && i >= 0) {
+            if (players[i] <= trainers[j]) {
+                result++;
+                i--;
+                j--;
+            } else {
+                i--;
+            }
+        }
+
+        return result;
+
+    }
+
+    public static void main(String[] args) {
+        int[] players = { 4, 7, 9 };
+        int[] trainers = { 8, 2, 5, 8 };
+
+        System.out.println("OUTPUT :  "
+                + new Maximum_Matching_Of_Players_With_Trainers().matchPlayersAndTrainers(players, trainers));
+
+    }
+}
